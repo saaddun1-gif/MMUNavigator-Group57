bdc9c47c95b3ae16c0e4b5f63700126a3da5eec2
Dania <dania07azizul@gmail.com>
2026-05-14 12:56:11 +0800

    flask function.

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')  # ← SEE THE 'template_folder' PART
 
 @app.route('/')
 def home():
-    return render_template('home_page.html')  # ← matches your file
+    return render_template('test.html')
 
 @app.route('/about')
 def about():
-    return render_template('about.html')       # ← matches your file
+    return render_template('about.html')
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html')     # ← matches your file
+    return render_template('contact.html')
+
+@app.route('/guide')
+def guide():
+    return render_template('guide.html')
 
 @app.route('/admin')
 def admin():
@@ -22,17 +26,9 @@ def admin():
 def admin_login():
     return render_template('Admin_Login.html')
 
-@app.route('/guide')
-def guide():
-    return render_template('guide.html')
-
 @app.route('/landing')
 def landing():
     return render_template('landing_page.html')
-
-@app.route('/mainweb')
-def mainweb():
-    return render_template('mainweb.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
